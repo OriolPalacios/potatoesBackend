506b88b63462eb99270b68bfb646d855e785275d
Oriol Fernando Palacios Durand <79288266+OriolPalacios@users.noreply.github.com>
2024-11-20 19:43:56 +0000

    backend draft

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Union
+from PIL import Image
+import io
+
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return "Conexion exitosa"
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# i want a post method that receives an image and returns the prediction of the image
+@app.post("/predict/")
+def predict(file: UploadFile = File(...)):
+    image = Image.open(io.BytesIO(file.file.read()))
+    # TODO: do something with the image
+    prediction = "whatever"
+    return {"prediction": prediction}
